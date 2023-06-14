a8d4b16c7bec117447a230a6438b0f95a8d177d8
Long Tran <lngtrn94@gmail.com>
2023-06-14 23:11:56 +0200

    show bookings in table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RollerService } from './roller/roller.service';
 import { SITES, Site } from './site/sites';
+import { Availabilities } from './roller/availabilities';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,45 @@ import { SITES, Site } from './site/sites';
 })
 export class AppComponent {
 
-  @Input() selectedDate: string = new Date().toDateString();
+  selectedDate: string = new Date().toDateString();
 
   sites = SITES;
   selectedSite: Site = SITES[0];
+
+  availabilities?: Availabilities;
+  loading = false;
+  sessionNames: string[] = [];
+  bookings: { [name: string]: { [sessionName: string]: number } } = {}
 
   constructor(private rollerService: RollerService) {
   }
 
   onChange() {
+    this.loading = true;
+    this.sessionNames = [];
+    this.bookings = {};
     this.rollerService.getAvailabilities(this.selectedSite, new Date(this.selectedDate)).subscribe(
-      console.log
+      avail => {
+        this.availabilities = avail;
+
+        const uniqueNames = new Set<string>();
+        avail.products?.forEach(p => {
+          const ticketName = p.name ?? '';
+          this.bookings[ticketName] = {};
+          p.sessions?.forEach(s => {
+            if (s.name) {
+              this.bookings[ticketName][s.name] = s.bookedQuantity ?? 0;
+              uniqueNames.add(s.name);
+            }
+          });
+        });
+
+        for (const name of uniqueNames) {
+          this.sessionNames.push(name);
+        }
+        this.sessionNames.sort();
+        this.loading = false;
+      }
     );
   }
 }
